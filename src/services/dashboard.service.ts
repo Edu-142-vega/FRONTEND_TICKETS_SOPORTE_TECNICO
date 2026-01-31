91963f83feb95b39f404d53135ad41a52972d16f
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2026-01-31 00:39:52 -0500

    url

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -1,26 +1,13 @@
-import axios from 'axios';
-
-const API_URL = 'http://127.0.0.1:3000';
+import { api } from '../api';
 
 export const dashboardService = {
   getStats: async () => {
-    const token = localStorage.getItem('token');
-    
-    const response = await axios.get(`${API_URL}/tickets/stats`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+    const response = await api.get('/tickets/stats');
     return response.data;
   },
 
   getAllTickets: async () => {
-    const token = localStorage.getItem('token');
-    const response = await axios.get(`${API_URL}/tickets/admin/all`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+    const response = await api.get('/tickets/admin/all');
     return response.data;
   }
 };
